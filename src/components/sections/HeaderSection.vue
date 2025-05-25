7621a7b5056f5dc4d25cac0164bafdceb30e27c4
trackcenter <ntrackcontact@gmail.com>
2025-05-25 02:01:03 +0000

    2.0

--- src/components/sections/HeaderSection.vue
+++ src/components/sections/HeaderSection.vue
@@ -47,7 +47,7 @@
                     class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
                 </li>
               </ul>
-              <div class="py-1">
+              <div class="py-1 drop">
                 <a href="#"
                   class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign
                   out</a>
